ea15625fd23dafa910f585650b930f78d87f6304
David Driscoll <david.driscoll@gmail.com>
2018-03-24 13:22:19 -0400

    Added generic ConventionContainer

--- src/Conventions.Abstractions/Scanners/IConventionContainer.cs
+++ src/Conventions.Abstractions/Scanners/IConventionContainer.cs
@@ -28,4 +28,31 @@ namespace Rocket.Surgery.Conventions.Scanners
         /// <param name="convention">The convention</param>
         void AppendConvention(IConvention convention);
     }
+
+    public interface IConventionContainer<out TBuilder, in TConvention, in TDelegate>
+    {
+        /// <summary>
+        /// Add a delegate to the scanner, that runs before scanning.
+        /// </summary>
+        /// <param name="delegate">The delegate</param>
+        TBuilder PrependDelegate(TDelegate @delegate);
+
+        /// <summary>
+        /// Adds a convention to the scanner, that runs before scanning.
+        /// </summary>
+        /// <param name="convention">The convention</param>
+        TBuilder PrependConvention(TConvention convention);
+
+        /// <summary>
+        /// Add a delegate to the scanner, that runs after scanning.
+        /// </summary>
+        /// <param name="delegate">The delegate</param>
+        TBuilder AppendDelegate(TDelegate @delegate);
+
+        /// <summary>
+        /// Adds a convention to the scanner, that runs after scanning.
+        /// </summary>
+        /// <param name="convention">The convention</param>
+        TBuilder AppendConvention(TConvention convention);
+    }
 }
