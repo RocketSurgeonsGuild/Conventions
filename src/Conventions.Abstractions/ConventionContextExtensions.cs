c31ebe86f5f2fa0886cf7988d6c80858aab553a6
David Driscoll <david.driscoll@gmail.com>
2019-09-25 23:12:57 -0400

    Added GetOrAdd methods for conventions to configure with default option elements +SemveR:minor

--- src/Conventions.Abstractions/ConventionContextExtensions.cs
+++ src/Conventions.Abstractions/ConventionContextExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Rocket.Surgery.Conventions
 {
     /// <summary>
@@ -25,6 +27,43 @@ namespace Rocket.Surgery.Conventions
 #nullable restore
 
         /// <summary>
+        /// Get a value by key from the context
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context">The context</param>
+        /// <param name="factory">The factory method in the event the type is not found</param>
+        /// <returns>T.</returns>
+        public static T GetOrAdd<T>(this IConventionContext context, Func<T> factory)
+            where T : class
+        {
+            if (!(context[typeof(T)] is T value))
+            {
+                value = factory();
+                context.Set(value);
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Get a value by key from the context
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context">The context</param>
+        /// <param name="key">The key where the value is saved</param>
+        /// <param name="factory">The factory method in the event the type is not found</param>
+        /// <returns>T.</returns>
+        public static T GetOrAdd<T>(this IConventionContext context, string key, Func<T> factory)
+            where T : class
+        {
+            if (!(context[key] is T value))
+            {
+                value = factory();
+                context.Set(value);
+            }
+            return value;
+        }
+
+        /// <summary>
         /// Get a value by type from the context
         /// </summary>
         /// <typeparam name="T">The type of the value</typeparam>
@@ -40,5 +79,11 @@ namespace Rocket.Surgery.Conventions
         /// <param name="key">The key where the value is saved</param>
         /// <param name="value">The value to save</param>
         public static void Set<T>(this IConventionContext context, string key, T value) => context[key] = value;
+
+        /// <summary>
+        /// Check if this is a test host (to allow conventions to behave differently during unit tests)
+        /// </summary>
+        /// <param name="context">The context</param>
+        public static bool IsTestHost(this IConventionContext context) => context.Get<HostType>() == HostType.TestHost;
     }
 }
---
+++ src/Conventions.Abstractions/HostType.cs
@@ -0,0 +1,11 @@
+namespace Rocket.Surgery.Conventions
+{
+    /// <summary>
+    /// The underlying host type (currently only checked for test host)
+    /// </summary>
+    internal enum HostType
+    {
+        Default,
+        TestHost
+    }
+}
