2f6bf2ac96599fc02ca8f2595d678621eefec473
David Driscoll <david.driscoll@gmail.com>
2020-07-13 20:57:18 -0400

    excluded some stuff from coverage as it just delegates to the underlying method

--- src/Conventions.TestHost/TestHostBuilder.cs
+++ src/Conventions.TestHost/TestHostBuilder.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -80,57 +81,83 @@ namespace Rocket.Surgery.Conventions
 #endif
 
         #region Interfaces
+        [ExcludeFromCodeCoverage]
         public IHostBuilder ConfigureHostConfiguration(Action<IConfigurationBuilder> configureDelegate) => _hostBuilder.ConfigureHostConfiguration(configureDelegate);
 
+        [ExcludeFromCodeCoverage]
         public IHostBuilder ConfigureAppConfiguration(Action<HostBuilderContext, IConfigurationBuilder> configureDelegate) => _hostBuilder.ConfigureAppConfiguration(configureDelegate);
 
+        [ExcludeFromCodeCoverage]
         public IHostBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configureDelegate) => _hostBuilder.ConfigureServices(configureDelegate);
 
+        [ExcludeFromCodeCoverage]
         public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory) => _hostBuilder.UseServiceProviderFactory(factory);
 
+        [ExcludeFromCodeCoverage]
         public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(Func<HostBuilderContext, IServiceProviderFactory<TContainerBuilder>> factory) => _hostBuilder.UseServiceProviderFactory(factory);
 
+        [ExcludeFromCodeCoverage]
         public IHostBuilder ConfigureContainer<TContainerBuilder>(Action<HostBuilderContext, TContainerBuilder> configureDelegate) => _hostBuilder.ConfigureContainer(configureDelegate);
 
+        [ExcludeFromCodeCoverage]
         public IDictionary<object, object> Properties => _hostBuilder.Properties;
+        [ExcludeFromCodeCoverage]
         public IConventionScanner Scanner => _conventionHostBuilder.Scanner;
 
+        [ExcludeFromCodeCoverage]
         public IAssemblyCandidateFinder AssemblyCandidateFinder => _conventionHostBuilder.AssemblyCandidateFinder;
 
+        [ExcludeFromCodeCoverage]
         public IServiceProviderDictionary ServiceProperties => _conventionHostBuilder.ServiceProperties;
 
+        [ExcludeFromCodeCoverage]
         public IAssemblyProvider AssemblyProvider => _conventionHostBuilder.AssemblyProvider;
 
+        [ExcludeFromCodeCoverage]
         public ILogger DiagnosticLogger => _conventionHostBuilder.DiagnosticLogger;
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder AppendConvention(IEnumerable<IConvention> conventions) => _conventionHostBuilder.AppendConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder AppendConvention(params IConvention[] conventions) => _conventionHostBuilder.AppendConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder AppendConvention(IEnumerable<Type> conventions) => _conventionHostBuilder.AppendConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder AppendConvention(params Type[] conventions) => _conventionHostBuilder.AppendConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder AppendConvention<T>()
             where T : IConvention => _conventionHostBuilder.AppendConvention<T>();
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder PrependConvention(IEnumerable<IConvention> conventions) => _conventionHostBuilder.PrependConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder PrependConvention(params IConvention[] conventions) => _conventionHostBuilder.PrependConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder PrependConvention(IEnumerable<Type> conventions) => _conventionHostBuilder.PrependConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder PrependConvention(params Type[] conventions) => _conventionHostBuilder.PrependConvention(conventions);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder PrependConvention<T>()
             where T : IConvention => _conventionHostBuilder.PrependConvention<T>();
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder AppendDelegate(IEnumerable<Delegate> delegates) => _conventionHostBuilder.AppendDelegate(delegates);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder AppendDelegate(params Delegate[] delegates) => _conventionHostBuilder.AppendDelegate(delegates);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder PrependDelegate(IEnumerable<Delegate> delegates) => _conventionHostBuilder.PrependDelegate(delegates);
 
+        [ExcludeFromCodeCoverage]
         public IConventionHostBuilder PrependDelegate(params Delegate[] delegates) => _conventionHostBuilder.PrependDelegate(delegates);
         #endregion
     }
