5f6b5ea9a53ccf27b708ec326daf0fc0a46980c8
David Driscoll <david.driscoll@gmail.com>
2022-07-03 00:29:48 -0400

    Updated default configuration to also export conventions from application assemblies.

---
+++ test/Analyzers.Tests/snapshots/ImportConventionsTests.Should_Support_Imports_And_Exports_In_The_Same_Assembly_If_Not_Exported.02Imported_Class_Conventions.verified.cs
@@ -0,0 +1,18 @@
+﻿//HintName: Imported_Class_Conventions.cs
+using System;
+using System.Collections.Generic;
+using Rocket.Surgery.Conventions;
+
+namespace TestProject
+{
+    public partial class Program
+    {
+        /// <summary>
+        /// The conventions imported into this assembly
+        /// </summary>
+        public static IEnumerable<IConventionWithDependencies> GetConventions(IServiceProvider serviceProvider)
+        {
+            yield break;
+        }
+    }
+}
