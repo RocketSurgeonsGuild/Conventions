719bcd3cd97e4e2f1c27722c73b89bfe47b6a99f
David Driscoll <david.driscoll@gmail.com>
2022-07-03 00:41:13 -0400

    Updated default configuration to also export conventions from application assemblies. (#885)

--- test/Analyzers.Tests/ImportConventionsTests.cs
+++ test/Analyzers.Tests/ImportConventionsTests.cs
@@ -1,4 +1,3 @@
-using Xunit;
 using static Rocket.Surgery.Conventions.Analyzers.Tests.GenerationHelpers;
 
 namespace Rocket.Surgery.Conventions.Analyzers.Tests;
@@ -152,5 +151,40 @@ namespace TestProject
 ";
 
         await Verify(GenerateAll(new[] { source1, source2 }));
+    }
+
+    [Fact]
+    public async Task Should_Support_Imports_And_Exports_In_The_Same_Assembly_If_Not_Exported()
+    {
+        var source1 = @"
+using Rocket.Surgery.Conventions;
+using Rocket.Surgery.Conventions.Tests;
+
+namespace Rocket.Surgery.Conventions.Tests
+{
+    [ExportConvention]
+    internal class Contrib : IConvention { }
+}
+";
+        var source2 = @"
+using Rocket.Surgery.Conventions;
+
+namespace TestProject
+{
+    [ImportConventions]
+    public partial class Program
+    {
+    }
+}
+";
+
+        await Verify(
+            GenerateAll(
+                new[] { source1, source2 }, properties: new Dictionary<string, string?>
+                {
+                    ["ExportConventionsAssembly"] = "false",
+                }
+            )
+        );
     }
 }
--- test/Analyzers.Tests/snapshots/ImportConventionsTests.Should_Support_Imports_And_Exports_In_The_Same_Assembly_If_Not_Exported.00Exported_Conventions.verified.cs
+++ test/Analyzers.Tests/snapshots/ImportConventionsTests.Should_Support_Imports_And_Exports_In_The_Same_Assembly_If_Not_Exported.00Exported_Conventions.verified.cs
@@ -8,20 +8,4 @@ using Rocket.Surgery.Conventions;
 [assembly: System.Reflection.AssemblyMetadata("Rocket.Surgery.ConventionConfigurationData.Exports.ClassName", "Exports")]
 [assembly: System.Reflection.AssemblyMetadata("Rocket.Surgery.ConventionConfigurationData.Exports.MethodName", "GetConventions")]
 [assembly: ExportedConventions(typeof(Rocket.Surgery.Conventions.Tests.Contrib))]
-namespace TestProject.Conventions
-{
-    /// <summary>
-    /// The class defined for exporting conventions from this assembly
-    /// </summary>
-    [System.Runtime.CompilerServices.CompilerGenerated]
-    public static partial class Exports
-    {
-        /// <summary>
-        /// The conventions exports from this assembly
-        /// </summary>
-        public static IEnumerable<IConventionWithDependencies> GetConventions(IServiceProvider serviceProvider)
-        {
-            yield return new ConventionWithDependencies(new Rocket.Surgery.Conventions.Tests.Contrib(), HostType.UnitTest);
-        }
-    }
-}
+[assembly: Convention(typeof(Rocket.Surgery.Conventions.Tests.Contrib))]
