8c258a909df886512e8f9f1ef5495630c7ac93a0
david-driscoll <david-driscoll@users.noreply.github.com>
2024-02-18 18:01:19 +0000

    Automatically linting code

--- test/Analyzers.Tests/GenerationHelpers.cs
+++ test/Analyzers.Tests/GenerationHelpers.cs
@@ -1,5 +1,3 @@
-using System.Runtime.Loader;
-using Microsoft.CodeAnalysis.CSharp;
 using Rocket.Surgery.Extensions.Testing.SourceGenerators;
 
 namespace Rocket.Surgery.Conventions.Analyzers.Tests;
