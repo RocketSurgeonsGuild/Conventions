38b524ba9e617ab64ec00ceca91ad124f4ff5ac6
David Driscoll <david.driscoll@gmail.com>
2025-09-13 22:27:03 -0400

    rename

---
+++ test/Analyzers.Tests/snapshots/ImportConventionsTests.Should_Support_Imports_And_Exports_In_The_Same_Assembly_If_Not_Exported#Exported_Conventions.g.verified.cs
@@ -0,0 +1,14 @@
+﻿//HintName: Rocket.Surgery.Conventions.Analyzers/Rocket.Surgery.Conventions.ConventionAttributesGenerator/Exported_Conventions.g.cs
+#nullable enable
+#pragma warning disable CA1002, CA1034, CA1822, CS0105, CS1573, CS8602, CS8603, CS8618, CS8669
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Rocket.Surgery.Conventions;
+
+[assembly: System.Reflection.AssemblyMetadata("Rocket.Surgery.ConventionConfigurationData.Exports.Namespace", "TestProject.Conventions")]
+[assembly: System.Reflection.AssemblyMetadata("Rocket.Surgery.ConventionConfigurationData.Exports.ClassName", "Exports")]
+[assembly: System.Reflection.AssemblyMetadata("Rocket.Surgery.ConventionConfigurationData.Exports.MethodName", "GetConventions")]
+[assembly: ExportedConventions(typeof(Rocket.Surgery.Conventions.Tests.Contrib))]
+#pragma warning restore CA1002, CA1034, CA1822, CS0105, CS1573, CS8602, CS8603, CS8618, CS8669
+#nullable restore
