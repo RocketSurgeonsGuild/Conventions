658e8cae77b86734863d76ea5353eccb89e680e5
Rocket Understudy <33589210+rsg-bot@users.noreply.github.com>
2026-03-02 20:53:51 -0500

    Automatically linting code

--- sample/Diagnostics/Program.cs
+++ sample/Diagnostics/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Hosting;
 using Rocket.Surgery.CommandLine;
 using Rocket.Surgery.Conventions;
 using Rocket.Surgery.Conventions.DependencyInjection;
-using Rocket.Surgery.Hosting;
 
 using Spectre.Console;
 using Spectre.Console.Cli;
